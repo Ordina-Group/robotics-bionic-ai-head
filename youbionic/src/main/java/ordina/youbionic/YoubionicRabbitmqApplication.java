f76307b0d363394f4b506f563b669b14ae4bce5f
Marten Elsinga <marten.elsinga@hotmail.com>
2023-10-27 12:28:08 +0200

    Initial Spring setup

---
+++ youbionic/src/main/java/ordina/youbionic/YoubionicRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package ordina.youbionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoubionicRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoubionicRabbitmqApplication.class, args);
+	}
+
+}
