f04b0a0a9c74d99d160792d033ef0e1c66fa699e
Marten Elsinga <marten.elsinga@hotmail.com>
2023-11-23 14:17:10 +0100

    Changed requestmapping to reflect what actually happens
    Changed GetMapping to PutMapping, as the requests change the state of the servomotors

--- message_hub/src/main/java/ordina/youbionic/presentation/ServoController.java
+++ message_hub/src/main/java/ordina/youbionic/presentation/ServoController.java
@@ -5,6 +5,7 @@ import ordina.youbionic.infrastructure.RabbitMQPublisher;
 import ordina.youbionic.service.ServoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,44 +26,44 @@ public class ServoController {
         return service.testAll();
     }
 
-    @GetMapping("/reset")
+    @PutMapping("/reset")
     public String reset() throws Exception{
         return service.reset();
     }
 
-    @GetMapping("/laugh")
+    @PutMapping("/laugh")
     public String laugh() throws Exception{
         return service.laugh();
     }
 
-    @GetMapping("/config/{servo}")
+    @PutMapping("/config/{servo}")
     public String config(@PathVariable String servo) throws Exception{
         return service.config(servo);
     }
 
-    @GetMapping("/manual/{servo}/{angle}")
+    @PutMapping("/manual/{servo}/{angle}")
     public String manual(@PathVariable String servo, @PathVariable String angle) throws Exception{
         return service.manual(servo, angle);
     }
 
-    @GetMapping("/rest")
+    @PutMapping("/rest")
     public String rest() throws Exception{
         return service.rest();
     }
 
-    @GetMapping("/blink")
+    @PutMapping("/blink")
     public String blink() throws Exception{
         service.blink();
         return "Blinked.";
     }
 
-    @GetMapping("/yes")
+    @PutMapping("/yes")
     public String yes() throws Exception{
         service.nodYes();
         return "Yes.";
     }
 
-    @GetMapping("/no")
+    @PutMapping("/no")
     public String no() throws Exception{
         service.shakeNo();
         return "No.";
