32e1875cdff26b415290470bced60d0aea83a514
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-10 12:09:36 +0100

    Update movement_data.py
    Rewritten to make it safer to edit

--- servo_driver/servo_driver/movement_data.py
+++ servo_driver/servo_driver/movement_data.py
@@ -17,18 +17,124 @@ class Position:
     headPivot: Optional[int]
 
 
-# TODO: Wanneer alle posities hier staan kan er een functie geschreven worden die
-# TODO: ervoor zorgt dat per pose alle values worden opgehaald en de servo's
-# TODO: worden aangestuurd daarmee. Dan kan je per servo vanaf de config de pinnr halen
-# TODO: en de rotaties hieruit halen. Als er 'None' staat, hoeft de servo niet te bewegen
-all90 = Position(90, 90, 90, 90, 90, 90, 90, 90, 90)
-rest = Position(conf.eyeLeft.defaultRotation, conf.eyeRight.defaultRotation, conf.eyeLeftOpen.defaultRotation, conf.eyeRightOpen.defaultRotation, conf.eyesUpDown.defaultRotation, conf.mouth.defaultRotation, conf.headTilt.defaultRotation, conf.headSwivel.defaultRotation, conf.headPivot.defaultRotation)
-closeEyes = Position(conf.eyeLeft.minRotation, conf.eyeRight.maxRotation, None, None, None, None, None, None, None)
-openEyes = Position(conf.eyeLeft.maxRotation, conf.eyeRight.minRotation, None, None, None, None, None, None, None)
-laughingEyeRoll = Position(110, 70, None, None, 70, None, 120, None, None)
-laughingPosition1 = Position(None, None, None, None, None, 90, 130, None, None)
-laughingPosition2 = Position(None, None, None, None, None, 70, 115, None, None)
-noddingYes1 = Position(None, None, None, None, None, None, 80, None, None)
-noddingYes2 = Position(None, None, None, None, None, None, 130, None, None)
-shakingNo1 = Position(None, None, None, None, None, None, None, 110, None)
-shakingNo2 = Position(None, None, None, None, None, None, None, 80, None)
+all90 = Position(
+    eyeLeft=90,
+    eyeRight=90,
+    eyeLeftOpen=90,
+    eyeRightOpen=90,
+    eyesUpDown=90,
+    mouth=90,
+    headTilt=90,
+    headSwivel=90,
+    headPivot=90
+)
+rest = Position(
+    eyeLeft=conf.eyeLeft.defaultRotation,
+    eyeRight=conf.eyeRight.defaultRotation,
+    eyeLeftOpen=conf.eyeLeftOpen.defaultRotation,
+    eyeRightOpen=conf.eyeRightOpen.defaultRotation,
+    eyesUpDown=conf.eyesUpDown.defaultRotation,
+    mouth=conf.mouth.defaultRotation,
+    headTilt=conf.headTilt.defaultRotation,
+    headSwivel=conf.headSwivel.defaultRotation,
+    headPivot=conf.headPivot.defaultRotation
+)
+closeEyes = Position(
+    eyeLeft=conf.eyeLeft.minRotation,
+    eyeRight=conf.eyeRight.maxRotation,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=None,
+    headSwivel=None,
+    headPivot=None
+)
+openEyes = Position(
+    eyeLeft=conf.eyeLeft.maxRotation,
+    eyeRight=conf.eyeRight.minRotation,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=None,
+    headSwivel=None,
+    headPivot=None
+)
+laughingEyeRoll = Position(
+    eyeLeft=110,
+    eyeRight=70,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=70,
+    mouth=None,
+    headTilt=120,
+    headSwivel=None,
+    headPivot=None
+)
+laughingPosition1 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=90,
+    headTilt=130,
+    headSwivel=None,
+    headPivot=None
+)
+laughingPosition2 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=70,
+    headTilt=115,
+    headSwivel=None,
+    headPivot=None
+)
+noddingYes1 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=80,
+    headSwivel=None,
+    headPivot=None
+)
+noddingYes2 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=130,
+    headSwivel=None,
+    headPivot=None
+)
+shakingNo1 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=None,
+    headSwivel=110,
+    headPivot=None
+)
+shakingNo2 = Position(
+    eyeLeft=None,
+    eyeRight=None,
+    eyeLeftOpen=None,
+    eyeRightOpen=None,
+    eyesUpDown=None,
+    mouth=None,
+    headTilt=None,
+    headSwivel=80,
+    headPivot=None
+)
