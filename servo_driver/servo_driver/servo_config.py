a1275fa7c2d149965245ecb68ff278e061cbe64c
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-10 13:05:29 +0100

    Update servo_config.py
    Added a name field

--- servo_driver/servo_driver/servo_config.py
+++ servo_driver/servo_driver/servo_config.py
@@ -7,14 +7,15 @@ class ServoMotor:
     defaultRotation: int
     minRotation: int
     maxRotation: int
+    name: String
 
 
-eyeLeft = ServoMotor(pinNr=0, defaultRotation=100, minRotation=80, maxRotation=115)
-eyeRight = ServoMotor(pinNr=1, defaultRotation=100, minRotation=80, maxRotation=115)
-eyeLeftOpen = ServoMotor(pinNr=2, defaultRotation=100, minRotation=80, maxRotation=120)
-eyeRightOpen = ServoMotor(pinNr=3, defaultRotation=100, minRotation=80, maxRotation=120)
-eyesUpDown = ServoMotor(pinNr=4, defaultRotation=105, minRotation=70, maxRotation=180)
-mouth = ServoMotor(pinNr=5, defaultRotation=70, minRotation=70, maxRotation=100)
-headTilt = ServoMotor(pinNr=13, defaultRotation=110, minRotation=70, maxRotation=150)
-headSwivel = ServoMotor(pinNr=14, defaultRotation=95, minRotation=50, maxRotation=130)
-headPivot = ServoMotor(pinNr=15, defaultRotation=95, minRotation=70, maxRotation=110)
+eyeLeft = ServoMotor(pinNr=0, defaultRotation=100, minRotation=80, maxRotation=115, name='eyeLeft')
+eyeRight = ServoMotor(pinNr=1, defaultRotation=100, minRotation=80, maxRotation=115, name='eyeRight')
+eyeLeftOpen = ServoMotor(pinNr=2, defaultRotation=100, minRotation=80, maxRotation=120, name='eyeLeftOpen')
+eyeRightOpen = ServoMotor(pinNr=3, defaultRotation=100, minRotation=80, maxRotation=120, name='eyeRightOpen')
+eyesUpDown = ServoMotor(pinNr=4, defaultRotation=105, minRotation=70, maxRotation=180, name='eyesUpDown')
+mouth = ServoMotor(pinNr=5, defaultRotation=70, minRotation=70, maxRotation=100, name='mouth')
+headTilt = ServoMotor(pinNr=13, defaultRotation=110, minRotation=70, maxRotation=150, name='headTilt')
+headSwivel = ServoMotor(pinNr=14, defaultRotation=95, minRotation=50, maxRotation=130, name='headSwivel')
+headPivot = ServoMotor(pinNr=15, defaultRotation=95, minRotation=70, maxRotation=110, name='headPivot')
