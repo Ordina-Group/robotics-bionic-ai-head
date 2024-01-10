d212620c67a98e93f02f24288137ea0d5b55c338
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-10 14:13:27 +0100

    Create sound_driver.py

---
+++ sound_driver/sound_driver.py
@@ -0,0 +1,42 @@
+import os
+import pika
+import sys
+import sounddevice as sd
+import numpy as np
+
+def main():
+    kit = ServoKit(channels=16)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='servo_output')
+    
+    # TODO - specify audio file path
+    audio_file_path = 'your_audio_file.wav'
+    
+    def callback(ch, method, properties, body):
+        command = body.decode.split(':')
+        if command[0] == 'play_sound':
+            audio_data, _ = sd.read(audio_file_path, dtype=np.int16)
+            sd.play(audio_data, samplerate=44100)
+            sd.wait()
+        elif command[0] == 'speak':
+            # Add functionality to generate text-to-speech and play the sound here
+            # using command[1] as the text to be said.
+        else:
+            print("Invalid command: %s" % (command[0]))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+    channel.basic_consume(queue='servo', on_message_callback=callback)
+
+    print(' [*] Waiting for messages. To exit, press CTRL+C')
+    channel.start_consuming()
+    
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
