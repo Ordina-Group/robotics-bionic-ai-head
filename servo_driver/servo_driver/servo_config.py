d910c3b5dcb57123c0d0ab4b5a9eec9c0b84bf03
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-10 13:02:35 +0100

    Update servo_config.py
    Made it more readable and editable

--- servo_driver/servo_driver/servo_config.py
+++ servo_driver/servo_driver/servo_config.py
@@ -9,12 +9,12 @@ class ServoMotor:
     maxRotation: int
 
 
-eyeLeft = ServoMotor(0, 100, 80, 115)
-eyeRight = ServoMotor(1, 100, 80, 115)
-eyeLeftOpen = ServoMotor(2, 100, 80, 120)
-eyeRightOpen = ServoMotor(3, 100, 80, 120)
-eyesUpDown = ServoMotor(4, 105, 70, 180)
-mouth = ServoMotor(5, 70, 70, 100)
-headTilt = ServoMotor(13, 110, 70, 150)
-headSwivel = ServoMotor(14, 95, 50, 130)
-headPivot = ServoMotor(15, 95, 70, 110)
+eyeLeft = ServoMotor(pinNr=0, defaultRotation=100, minRotation=80, maxRotation=115)
+eyeRight = ServoMotor(pinNr=1, defaultRotation=100, minRotation=80, maxRotation=115)
+eyeLeftOpen = ServoMotor(pinNr=2, defaultRotation=100, minRotation=80, maxRotation=120)
+eyeRightOpen = ServoMotor(pinNr=3, defaultRotation=100, minRotation=80, maxRotation=120)
+eyesUpDown = ServoMotor(pinNr=4, defaultRotation=105, minRotation=70, maxRotation=180)
+mouth = ServoMotor(pinNr=5, defaultRotation=70, minRotation=70, maxRotation=100)
+headTilt = ServoMotor(pinNr=13, defaultRotation=110, minRotation=70, maxRotation=150)
+headSwivel = ServoMotor(pinNr=14, defaultRotation=95, minRotation=50, maxRotation=130)
+headPivot = ServoMotor(pinNr=15, defaultRotation=95, minRotation=70, maxRotation=110)
