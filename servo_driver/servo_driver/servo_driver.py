f2e48a5c588f04b1010a0afe783c24f3ee86d83d
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-10 13:42:06 +0100

    Update servo_driver.py
    This should fix the driver again!

--- servo_driver/servo_driver/servo_driver.py
+++ servo_driver/servo_driver/servo_driver.py
@@ -1,31 +1,56 @@
 import os
 import pika
 import sys
-import servo_config
+import servo_config as config
 from adafruit_servokit import ServoKit
 import movement_data
-from dataclasses import dataclass, fields
-import asyncio
+from dataclasses import dataclass, fields, asdict
+# import asyncio
+# import aio_pika
+# I have these commented out, as to indicate a desire to make everything asynchronous. This way, the head can move more naturally. As for now, it's a sequential driver, as I'm struggling to understand asynchronity.
 
 
-async def main():
+def main():
     kit = ServoKit(channels=16)
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='servo')
+    
+    # Here we define what possible commands can be sent to the driver.
+    # If future developers add new methods, make sure to add them to this dictionary.
+    commands = {
+        'rest': rest,
+        'close_eyes': close_eyes,
+        'open_eyes': open_eyes,
+        'all90': all90,
+        'nod_yes': nod_yes,
+        'shake_no': shake_no,
+        'blink': blink,
+        'laugh': laugh
+    }
 
     def callback(ch, method, properties, body):
-        # instructions = body.decode().split(',')
-        # print(f" [x] Received {instructions}")
+        instructions = body.decode().split(':')
+        print(f" [x] Received {instructions}")
+        if instructions[0] == "speak":
+            speak(instructions[1])
+        elif instructions[0] in commands:
+            commands[instructions[0]]()
+        elif instructions[0] == "manualWithName":
+            manualWithName(instructions[1], instructions[2])
+        elif instructions[0] == "manualWithNumber":
+            manualWithNumber(instructions[1], instructions[2])
+        elif instructions[0] == "config":
+            config(instructions[1])
+        else:
+            print("Unknown command: %s" % (instructions[0]))
         ch.basic_ack(delivery_tag=method.delivery_tag)
-
+    
     def move(pos):
-        i = 0
-        for field in fields(pos):
-            angle = getattr(pos, field.name)
-            if angle is not None:
-                kit.servo[i].angle = angle
-            i += 1
+        for field, value in asdict(pos).items:
+            if value is not None:
+                pinNumber = findPinNumber(field)
+                kit.servo[pinNumber].angle = value
 
     def rest():
         move(movement_data.rest)
@@ -39,58 +64,74 @@ async def main():
     def all90():
         move(movement_data.all90)
 
-    async def nod_yes():
+    def nod_yes():
         close_eyes()
         i = 0
         while i < 5:
             move(movement_data.noddingYes1)
-            await asyncio.sleep(0.4)
+            sleep(0.4)
             move(movement_data.noddingYes2)
-            await asyncio.sleep(0.4)
+            sleep(0.4)
         open_eyes()
 
-    async def shake_no():
+    def shake_no():
         close_eyes()
         i = 0
         while i < 5:
             move(movement_data.shakingNo1)
-            await asyncio.sleep(0.4)
+            sleep(0.4)
             move(movement_data.shakingNo2)
-            await asyncio.sleep(0.4)
+            sleep(0.4)
         open_eyes()
 
-    async def blink():
+    def blink():
         close_eyes()
-        await asyncio.sleep(0.25)
+        sleep(0.25)
         open_eyes()
 
-    async def laugh():
+    def laugh():
         rest()
-        await asyncio.sleep(0.1)
+        sleep(0.1)
         move(movement_data.laughingEyeRoll)
-        await asyncio.sleep(0.2)
+        sleep(0.2)
         close_eyes()
         i = 0
         while i < 8:
             move(movement_data.laughingPosition1)
-            await asyncio.sleep(0.2)
+            sleep(0.2)
             move(movement_data.laughingPosition2)
-            await asyncio.sleep(0.2)
+            sleep(0.2)
             i += 1
         rest()
 
-    def manual(servo_number, angle):
+    def manualWithName(servoName, angle):
+        servo_number = findPinNumber(servoName)
         kit.servo[servo_number].angle = angle
 
-    async def configure(servo_number):
+    def manualWithNumber(servo_number, angle):
+        kit.servo[servo_number].angle = angle
+
+    def configure(servo_number):
         i = 0
         while i < 2:
             kit.servo[servo_number].angle = 80
-            await asyncio.sleep(0.5)
+            sleep(0.5)
             kit.servo[servo_number].angle = 110
-            await asyncio.sleep(0.5)
+            sleep(0.5)
             i += 1
         kit.servo[servo_number].angle = 90
+    
+    def speak(text):
+        # If the robot head were to ever speak, this method would make that happen. 
+        # However, as this is outside of the scope of this current assignment,
+        # it is just an empty method for now.
+        sleep(2)
+        
+    def findPinNumber(servoMotorName):
+        servoMotors = [config.eyeLeft, config.eyeRight, config.eyeLeftOpen, config.eyeRightOpen, config.eyesUpDown, config.mouth, config.headTilt, config.headSwivel, config.headPivot]
+        for servoMotor in servoMotors:
+            if servoMotorName == servoMotor.name:
+                return servoMotor.pinNr
 
     channel.basic_consume(queue='servo', on_message_callback=callback)
 
@@ -100,7 +141,7 @@ async def main():
 
 if __name__ == '__main__':
     try:
-        asyncio.run(main())
+        main()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
