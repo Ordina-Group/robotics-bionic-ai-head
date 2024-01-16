55212557c9cabf6719fe8c42055d4ba0b8631a49
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-16 12:58:57 +0100

    Create Button.svelte

---
+++ frontend/svelte-app/Button.svelte
@@ -0,0 +1,21 @@
+<script>
+  let buttonProps = {
+  class:[$$restProps.class]
+  }
+</script>
+  <button on:click
+      on:mouseover
+      on:mouseenter
+      on:mouseleave
+    {...buttonProps}>
+      <slot/>
+    </button>
+
+<style>
+  .primary{
+    color:blue;
+  }
+  .lg{
+    font-size:2em;
+  }
+</style>
