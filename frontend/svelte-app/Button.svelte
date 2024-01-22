3e5dd5a357f7f84cfd313b6d7b37a97665fd7d98
Marten Elsinga <74406658+Martenelsinga@users.noreply.github.com>
2024-01-22 12:58:26 +0100

    Update Button.svelte

--- frontend/svelte-app/Button.svelte
+++ frontend/svelte-app/Button.svelte
@@ -1,4 +1,6 @@
 <script>
+  let isHovered = false;
+
   let buttonProps = {
   class:[$$restProps.class]
   }
@@ -14,10 +16,23 @@
     </button>
 
 <style>
-  .primary{
-    color:blue;
+  .primary {
+    font-family: 'Roboto';
+    background-color: #007f92;
+    color: #3da4bf;
+    transition: background-color 0.4s, color 0.2s;
+	border-color: #e1e1e1;
+	border-radius: 8px;
+	border-width: 2px;
+	margin: 2px;
+	font-size: 2em;
+	width: 5em;
+	padding-right: 1em;
+	padding-left: 1em;
   }
-  .lg{
-    font-size:2em;
+
+  .primary:hover {
+    background-color: #e1e1e1;
+    color: #007f92;
   }
 </style>
