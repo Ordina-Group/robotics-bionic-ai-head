f6e0c7f26fbfb749d606e541064dad919bb76d51
Marten Elsinga <marten.elsinga@hotmail.com>
2024-04-17 16:34:25 +0200

    Update online_wit_test.py

--- speech_driver/proofs-of-concept/online_wit_test.py
+++ speech_driver/proofs-of-concept/online_wit_test.py
@@ -39,32 +39,32 @@ with sr.Microphone() as source:
         try:
             spokenText = r.recognize_wit(audio, key=witKey).lower()
             end = time.time()
-        print(f"Spraak herkend in {end - start} seconden")
-        mistakeList = ["ordinna", "ordeena", "gopelt", "robelt", "oortina", "hopelt", "globalte", "vanopot", "ortina", "reelbot", "oordinnen"]
-        correctedList = ["ordina", "ordina", "robot", "robot", "ordina", "robot", "robot", "robot", "ordina", "robot", "ordina"]
-        # With the introduction of 'initial_prompt' in the r.recognize_whisper, mistake correction becomes almost unnecessary. 
-        # However, it is still applied here to improve performance.
-        for i in range(len(mistakeList)):
-            textList = spokenText.split()
-            for y in range(len(textList)):
-                if textList[y] == mistakeList[i]:
-                    textList[y] = correctedList[i]
-            fixedText = " ".join(str(word) for word in textList)
-        if "robot" in fixedText or awake == True:
-            awake = True
-            answered = False
-            for keyword in keywords:
-                if keyword in fixedText and answered == False:
-                    answered = True
-                    if keyword == "spraak" or keyword == "vraag":
-                        awake = False
-                        method = getattr(activationFunction, keyword)
-                        method(fixedText)
-                    else:
-                        awake = False
-                        method = getattr(activationFunction, keyword)
-                        method()
-        print(fixedText)
+            print(f"Spraak herkend in {end - start} seconden")
+            mistakeList = ["ordinna", "ordeena", "gopelt", "robelt", "oortina", "hopelt", "globalte", "vanopot", "ortina", "reelbot", "oordinnen"]
+            correctedList = ["ordina", "ordina", "robot", "robot", "ordina", "robot", "robot", "robot", "ordina", "robot", "ordina"]
+            # With the introduction of 'initial_prompt' in the r.recognize_whisper, mistake correction becomes almost unnecessary. 
+            # However, it is still applied here to improve performance.
+            for i in range(len(mistakeList)):
+                textList = spokenText.split()
+                for y in range(len(textList)):
+                    if textList[y] == mistakeList[i]:
+                        textList[y] = correctedList[i]
+                fixedText = " ".join(str(word) for word in textList)
+            if "robot" in fixedText or awake == True:
+                awake = True
+                answered = False
+                for keyword in keywords:
+                    if keyword in fixedText and answered == False:
+                        answered = True
+                        if keyword == "spraak" or keyword == "vraag":
+                            awake = False
+                            method = getattr(activationFunction, keyword)
+                            method(fixedText)
+                        else:
+                            awake = False
+                            method = getattr(activationFunction, keyword)
+                            method()
+            print(fixedText)
         except sr.UnknownValueError:
             print("Something went wrong")
         
