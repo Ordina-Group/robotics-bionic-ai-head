150cb369e1b3a6c45183ab3e23f9568cc3f2a908
Marten Elsinga <marten.elsinga@hotmail.com>
2024-06-18 12:53:11 +0200

    little cleanup

--- start.sh
+++
@@ -1,4 +0,0 @@
-python ./speech_driver/speech_driver/speech_driver.py & 
-python ./servo_driver/servo_driver/servo_driver.py &
-python ./sound_driver/sound_driver/sound_driver.py &
-python ./message_hub/message_hub/message_hub.py &
--- stop.sh
+++
@@ -1,7 +0,0 @@
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     pkill -9 -f .py;;
-    *)          TASKKILL //F //IM python*
-esac
-
-
